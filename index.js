45c6873b0f96237a4965b7a545733eb54965589f
keatonshall <keatonshall314@gmail.com>
2023-12-25 18:21:01 -0600

    Tic-Tac-Toe prototype, no rules, basic styling

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
-function setValue(){
-  let url = window.location.href;
-  document.getElementById("value").innerHTML = (url.split("?")[1] != undefined)? url.split("?")[1] : 20;
-}
-
-
-function takeOff(n){
-  document.getElementById("value").innerHTML = Number(document.getElementById("value").innerHTML) - n;
-}
-
+let board = [];
+let userMove = -1; //Single move selected by User, -1 by default
+let turn = "X";
 
 function send(){
-  let value = document.getElementById("value").innerHTML
-  let url = window.location.href.split("?")[0] + "?" + value;
-  console.log(url);
-  if (navigator.share) {
-    navigator.share({
-        title: 'Game',
-        url: url,
-      })
-      .then(() => console.log('Successful share'))
-      .catch((error) => console.log('Error sharing', error));
-  } else {
-    console.log('Share not supported on this browser, do it the old way.');
+  if(userMove > -1){
+    let newURL = window.location.href + userMove;
+    console.log(newURL);
+    if (navigator.share) {
+      navigator.share({
+          title: 'Tic-Tac-Toe',
+          url: newURL,
+        })
+    }
   }
+
+}
+
+
+function select(cell){
+  console.log(cell);
+  if(!board[cell].locked){
+    //Delete all unlocked cells
+    for(let i = 0; i < 9; i++){
+      if(!board[i].locked){
+        board[i].piece = "";
+      }
+    }
+    board[cell].piece = turn;
+    userMove = cell; //Change user
+    showState();
+  }
+}
+
+
+function showState(){
+  for(let i = 0; i < 9; i++){
+    document.getElementById("b" + i).innerHTML = board[i].piece;
+  }
+}
+
+
+function initializeFromURL(){
+  let url = window.location.href;
+  state = url.split("?")[1]; //Extract turn sequence
+  //Initalize board for all empty;
+  for(let i = 0; i < 9; i++){
+    board.push({
+      piece: "",
+      locked: false
+    });
+  }
+
+  for(let i = 0; i < state.length; i++){//Loop over turn sequence
+    let move = Number(state.at(i));
+    board[move].piece = turn;
+    board[move].locked = true; //Lock after move is placed
+    turn = switchTurn(turn);
+  }
+  showState();
+}
+
+
+function switchTurn(t){
+  return (t == "X")? "O" : "X";
 }
